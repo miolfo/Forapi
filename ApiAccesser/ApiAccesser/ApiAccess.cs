878cab12f1fc50cbcdf27281703ac73a5cf1e520
Mikko Forsman <mikko.o.forsman@gmail.com>
2015-06-02 00:01:21 +0300

    Public getters and setters

--- ApiAccesser/ApiAccesser/ApiAccess.cs
+++ ApiAccesser/ApiAccesser/ApiAccess.cs
@@ -12,19 +12,74 @@ namespace ApiAccesser
 {
     public class ApiAccess
     {
+        /*PRIVATE VARIABLES*/
         private static HttpResponseMessage response;
         private string baseUri;
         private string requestUri;
         private string responseString;
 
+        /*PUBLIC GETTERS AND SETTERS*/
+        public string BaseUri
+        {
+            get
+            {
+                return baseUri;
+            }
+            set
+            {
+                //TODO: parse valid url
+                baseUri = value;
+            }
+        }
+
+        public string RequestUri
+        {
+            get
+            {
+                return requestUri;
+            }
+            set
+            {
+                //TODO: parse valid url
+                requestUri = value;
+            }
+        }
+
+        public string ResponseString
+        {
+            get
+            {
+                return responseString;
+            }
+        }
+
+        public string HttpResponse
+        {
+            get
+            {
+                return response.ToString();
+            }
+        }
+
+        /*PUBLIC METHODS*/
         public ApiAccess(string baseURI, string requestURI)
         {
             baseUri = baseURI;
             requestUri = requestURI;
         }
 
+        //Public method for accessing api
+        public async Task<string> GetApiResponseAsync()
+        {
+            string parsedResponse = "";
+            string[] res = await ApiCallAsync();
+
+            return res[1];
+        }
+
         
-        //TODO: privatize
+
+        /*PRIVATE METHODS*/
         private async Task<string[]> ApiCallAsync()
         {
             using (var client = new HttpClient())
@@ -39,14 +94,7 @@ namespace ApiAccesser
             return responseArray;
         }
 
-        //Public method for accessing api
-        public async Task<string> GetApiResponseAsync()
-        {
-            string parsedResponse = "";
-            string[] res = await ApiCallAsync();
 
-            return res[1];
-        }
     }
 
 }
